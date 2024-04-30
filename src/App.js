5c6a6f8a3a34ad95824888b50402bf09b16a5610
saramohamed124 <saramohamaden1234@gmail.com>
2024-05-01 01:24:12 +0300

    route

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
       <NavbarReact/> 
 <Routes>
   <Route path='/' element={<Home/>}></Route>
+  <Route path='/gold-app' element={<Home/>}></Route>
   <Route path='shop' element={<Shop/>}></Route>
   <Route path='cart' element={<Cart/>}></Route>
   <Route path='checkout' element={<Checkout/>}></Route>
